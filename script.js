8b5e58d0742e41c0585b0cd69b582994d987a326
Randall Cox <rcoxwork@gmail.com>
2020-11-16 12:19:02 -0800

    Build countdown

--- script.js
+++ script.js
@@ -83,6 +83,27 @@ function createEquations() {
 //   itemContainer.appendChild(bottomSpacer);
 // }
 
+// Displays 3, 2, 1, GO!
+function countdownStart() {
+  countdown.textContent = '3';
+  setTimeout(() => {
+    countdown.textContent = '2';
+  }, 1000);
+  setTimeout(() => {
+    countdown.textContent = '1';
+  }, 2000);
+  setTimeout(() => {
+    countdown.textContent = 'GO!';
+  }, 3000);
+}
+
+// Navigate from splash page to countdown page
+function showCountdown() {
+  countdownPage.hidden = false;
+  splashPage.hidden = true;
+  countdownStart();
+}
+
 // Get the value from our selected radio button
 function getRadioValue() {
   let radioValue;
@@ -100,6 +121,9 @@ function selectQuestionAmount(e) {
   e.preventDefault();
   questionAmount = getRadioValue();
   console.log('question amount: ', questionAmount);
+  if (questionAmount) {
+    showCountdown();
+  }
 }
 
 // Event listeners
