ab8457bbcd0bf20dd721a34b13299ffb4c655c1d
Randall Cox <rcoxwork@gmail.com>
2020-11-16 15:30:00 -0800

    Remove cross-function dependencies and rationalize code sequencing

--- script.js
+++ script.js
@@ -249,16 +249,28 @@ function populateGamePage() {
 
 // Displays 3, 2, 1, GO!
 function countdownStart() {
-  countdown.textContent = '3';
-  setTimeout(() => {
-    countdown.textContent = '2';
+  let count = 3;
+  countdown.textContent = count;
+  const timeCountDown = setInterval(() => {
+    count--;
+    if (count === 0) {
+      countdown.textContent = 'GO!';
+    } else if (count === -1) {
+      showGamePage();
+      clearInterval(timeCountDown);
+    } else {
+      countdown.textContent = count;
+    }
   }, 1000);
-  setTimeout(() => {
-    countdown.textContent = '1';
-  }, 2000);
-  setTimeout(() => {
-    countdown.textContent = 'GO!';
-  }, 3000);
+  // setTimeout(() => {
+  //   countdown.textContent = '2';
+  // }, 1000);
+  // setTimeout(() => {
+  //   countdown.textContent = '1';
+  // }, 2000);
+  // setTimeout(() => {
+  //   countdown.textContent = 'GO!';
+  // }, 3000);
 }
 
 // Navigate from splash page to countdown page
@@ -267,7 +279,7 @@ function showCountdown() {
   splashPage.hidden = true;
   countdownStart();
   populateGamePage();
-  setTimeout(showGamePage, 4000);
+  // setTimeout(showGamePage, 4000);
 }
 
 // Get the value from our selected radio button
